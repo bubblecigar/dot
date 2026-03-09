66cba4a9acaa8cb3c0233a00e5fffea489fd61bb
roy.wang <roy.wang@goanimate.com>
2026-03-09 16:09:27 +0800

    Render server random number in debug layer

--- client/scripts/controllers/server_number_label.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-func _ready() -> void:
-	text = "Waiting for server number..."
-	ClientRpc.random_number_received.connect(_on_random_number_received)
-
-func _on_random_number_received(value: int) -> void:
-	text = str(value)
--- client/scripts/debugs/debug_layer.gd
+++ client/scripts/debugs/debug_layer.gd
@@ -1,8 +1,11 @@
 extends CanvasLayer
 
 @onready var debug_label: Label = $DebugLabel
+var _latest_random_number: String = "N/A"
 
 func _ready() -> void:
+	ClientRpc.random_number_received.connect(_on_random_number_received)
+
 	if not OS.is_debug_build():
 		visible = false
 		set_process(false)
@@ -12,8 +15,14 @@ func _process(_delta: float) -> void:
 	var keys := snapshot.keys()
 	keys.sort()
 
-	var lines: PackedStringArray = ["StateStore"]
+	var lines: PackedStringArray = [
+		"StateStore",
+		"server_random_number: %s" % _latest_random_number,
+	]
 	for key in keys:
 		lines.append("%s: %s" % [key, var_to_str(snapshot[key])])
 
 	debug_label.text = "\n".join(lines)
+
+func _on_random_number_received(value: int) -> void:
+	_latest_random_number = str(value)
