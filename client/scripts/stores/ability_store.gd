3556c2d310035cd23b16d91424b6b60192bbc0c9
roy.wang <roy.wang@goanimate.com>
2026-03-09 17:58:19 +0800

    Replace ability tree menu with U-triggered card selection

--- client/scripts/stores/ability_store.gd
+++ client/scripts/stores/ability_store.gd
@@ -1,160 +1,22 @@
 extends Node
 
-signal ability_tree_changed
-signal ability_unlocked(ability_id: String)
+signal selected_cards_changed
 
-var ability_tree: Dictionary = {}
-var unlocked_abilities: Dictionary = {}
-var expanded_nodes: Dictionary = {}
-var _generated_node_index: int = 1
+var selected_cards: PackedInt32Array = PackedInt32Array()
 
-func _ready() -> void:
-	_set_default_tree()
+func add_selected_card(value: int) -> void:
+	selected_cards.append(value)
+	selected_cards_changed.emit()
 
-func _set_default_tree() -> void:
-	var default_tree := {
-		"root": {
-			"name": "Core",
-			"requires": PackedStringArray(),
-			"children": PackedStringArray(),
-		},
-	}
+func get_selected_cards() -> PackedInt32Array:
+	return selected_cards.duplicate()
 
-	set_ability_tree(default_tree, PackedStringArray(["root"]))
-
-func set_ability_tree(tree: Dictionary, initially_unlocked: PackedStringArray = PackedStringArray()) -> void:
-	var normalized_tree := _normalize_tree(tree)
-	if normalized_tree.is_empty():
-		return
-
-	ability_tree = normalized_tree
-	unlocked_abilities.clear()
-	expanded_nodes.clear()
-	for ability_id in initially_unlocked:
-		if ability_tree.has(ability_id):
-			unlocked_abilities[ability_id] = true
-
-	ability_tree_changed.emit()
-
-func has_ability(ability_id: String) -> bool:
-	return ability_tree.has(ability_id)
-
-func is_unlocked(ability_id: String) -> bool:
-	return bool(unlocked_abilities.get(ability_id, false))
-
-func can_unlock(ability_id: String) -> bool:
-	if not ability_tree.has(ability_id):
-		return false
-	if is_unlocked(ability_id):
-		return false
-
-	var node_data: Dictionary = ability_tree[ability_id]
-	var requires: PackedStringArray = node_data.get("requires", PackedStringArray())
-	for required_id in requires:
-		if not is_unlocked(required_id):
-			return false
-	return true
-
-func unlock_ability(ability_id: String) -> bool:
-	if not can_unlock(ability_id):
-		return false
-
-	unlocked_abilities[ability_id] = true
-	ability_unlocked.emit(ability_id)
-	ability_tree_changed.emit()
-	return true
-
-func add_random_children(ability_id: String, min_children: int = 3, max_children: int = 6) -> int:
-	if not ability_tree.has(ability_id):
-		return 0
-	if has_expanded(ability_id):
-		return 0
-
-	var min_count := mini(min_children, max_children)
-	var max_count := maxi(min_children, max_children)
-	var child_count := randi_range(min_count, max_count)
-
-	var parent_node: Dictionary = ability_tree[ability_id]
-	var children: PackedStringArray = parent_node.get("children", PackedStringArray())
-
-	for _i in range(child_count):
-		var child_id := _create_unique_ability_id(ability_id)
-		var child_name := child_id.capitalize()
-		ability_tree[child_id] = {
-			"name": child_name,
-			"requires": PackedStringArray([ability_id]),
-			"children": PackedStringArray(),
-		}
-		children.append(child_id)
-
-	parent_node["children"] = children
-	ability_tree[ability_id] = parent_node
-	expanded_nodes[ability_id] = true
-	ability_tree_changed.emit()
-	return child_count
-
-func has_expanded(ability_id: String) -> bool:
-	return bool(expanded_nodes.get(ability_id, false))
-
-func get_ability_node(ability_id: String) -> Dictionary:
-	if not ability_tree.has(ability_id):
-		return {}
-	return (ability_tree[ability_id] as Dictionary).duplicate(true)
-
-func get_ability_ids() -> PackedStringArray:
-	var ids: PackedStringArray = PackedStringArray()
-	for ability_id in ability_tree.keys():
-		ids.append(String(ability_id))
-	return ids
-
-func get_unlocked_ability_ids() -> PackedStringArray:
-	var ids: PackedStringArray = PackedStringArray()
-	for ability_id in unlocked_abilities.keys():
-		ids.append(String(ability_id))
-	return ids
-
-func get_ability_tree() -> Dictionary:
-	return ability_tree.duplicate(true)
+func clear_selected_cards() -> void:
+	selected_cards.clear()
+	selected_cards_changed.emit()
 
 func get_debug_snapshot() -> Dictionary:
 	return {
-		"ability_count": ability_tree.size(),
-		"unlocked_count": unlocked_abilities.size(),
-		"expanded_count": expanded_nodes.size(),
-		"unlocked_abilities": get_unlocked_ability_ids(),
+		"selected_card_count": selected_cards.size(),
+		"selected_cards": get_selected_cards(),
 	}
-
-func _normalize_tree(tree: Dictionary) -> Dictionary:
-	var normalized := {}
-
-	for ability_id_variant in tree.keys():
-		var ability_id := String(ability_id_variant)
-		var node_data_variant: Variant = tree[ability_id_variant]
-		if typeof(node_data_variant) != TYPE_DICTIONARY:
-			continue
-
-		var node_data: Dictionary = node_data_variant
-		var requires: PackedStringArray = PackedStringArray()
-		for required_variant in node_data.get("requires", PackedStringArray()):
-			requires.append(String(required_variant))
-
-		var children: PackedStringArray = PackedStringArray()
-		for child_variant in node_data.get("children", PackedStringArray()):
-			children.append(String(child_variant))
-
-		normalized[ability_id] = {
-			"name": String(node_data.get("name", ability_id.capitalize())),
-			"requires": requires,
-			"children": children,
-		}
-
-	return normalized
-
-func _create_unique_ability_id(parent_id: String) -> String:
-	var child_id := ""
-	while true:
-		child_id = "%s_child_%d" % [parent_id, _generated_node_index]
-		_generated_node_index += 1
-		if not ability_tree.has(child_id):
-			return child_id
-	return ""
--- client/scripts/ui/ability_tree_menu.gd
+++ client/scripts/ui/ability_tree_menu.gd
@@ -1,346 +1,58 @@
 extends Control
 
-const NODE_DIAMETER := 34.0
-const EDGE_LENGTH := 120.0
-const ROOT_SPAN := PI * 0.9
-const MIN_SPAN := PI / 9.0
-const CANVAS_PADDING := 80.0
+@onready var cards_row: HBoxContainer = $Layout/CardsRow
+@onready var selected_label: Label = $Layout/SelectedLabel
 
-@onready var graph_edges: Control = $GraphEdges
-@onready var graph_nodes: Control = $GraphNodes
-
-var _node_positions: Dictionary = {}
+var _offered_cards: PackedInt32Array = PackedInt32Array()
 
 func _ready() -> void:
-	AbilityStore.ability_tree_changed.connect(_refresh_ability_tree_menu)
-	resized.connect(_refresh_ability_tree_menu)
-	_refresh_ability_tree_menu.call_deferred()
+	AbilityStore.selected_cards_changed.connect(_refresh_selected_label)
+	_refresh_selected_label()
 
-func _refresh_ability_tree_menu() -> void:
-	for child in graph_edges.get_children():
+func _unhandled_input(event: InputEvent) -> void:
+	if not _is_menu_visible():
+		return
+	if event is InputEventKey:
+		var key_event := event as InputEventKey
+		if key_event.pressed and not key_event.echo and key_event.keycode == KEY_U:
+			_generate_cards()
+			get_viewport().set_input_as_handled()
+
+func _generate_cards() -> void:
+	_offered_cards.clear()
+	for i in range(3):
+		_offered_cards.append(randi_range(1, 99))
+	_render_cards()
+
+func _render_cards() -> void:
+	for child in cards_row.get_children():
 		child.queue_free()
-	for child in graph_nodes.get_children():
-		child.queue_free()
 
-	var tree: Dictionary = AbilityStore.get_ability_tree()
-	if tree.is_empty():
-		_add_graph_message("Ability tree is empty")
-		return
-
-	var layout_root := "root"
-	if not tree.has(layout_root):
-		layout_root = _get_fallback_focus(tree)
-	_sync_saved_positions(tree, layout_root)
-
-	var render_positions := _node_positions.duplicate(true)
-	_fit_positions_to_canvas(render_positions)
-	_draw_graph_edges(tree, render_positions)
-	_draw_graph_nodes(tree, render_positions)
-
-func _get_fallback_focus(tree: Dictionary) -> String:
-	for ability_id_variant in tree.keys():
-		return String(ability_id_variant)
-	return ""
-
-func _calculate_node_positions(tree: Dictionary, root_id: String) -> Dictionary:
-	var positions := {}
-	if root_id == "" or not tree.has(root_id):
-		return positions
-
-	positions[root_id] = Vector2.ZERO
-	var subtree_weights := {}
-	_compute_subtree_weight(tree, root_id, subtree_weights, {})
-	_assign_by_angle(
-		tree,
-		root_id,
-		-PI * 0.5,
-		ROOT_SPAN,
-		subtree_weights,
-		positions,
-		{}
-	)
-	return positions
-
-func _sync_saved_positions(tree: Dictionary, layout_root: String) -> void:
-	_prune_saved_positions(tree)
-
-	if _node_positions.is_empty():
-		_node_positions = _calculate_node_positions(tree, layout_root)
-		return
-
-	_append_missing_positions(tree)
-
-func _prune_saved_positions(tree: Dictionary) -> void:
-	var remove_ids: Array[String] = []
-	for ability_id_variant in _node_positions.keys():
-		var ability_id := String(ability_id_variant)
-		if not tree.has(ability_id):
-			remove_ids.append(ability_id)
-
-	for ability_id in remove_ids:
-		_node_positions.erase(ability_id)
-
-func _append_missing_positions(tree: Dictionary) -> void:
-	var pending_ids: Array[String] = []
-	for ability_id_variant in tree.keys():
-		var ability_id := String(ability_id_variant)
-		if not _node_positions.has(ability_id):
-			pending_ids.append(ability_id)
-	pending_ids.sort()
-
-	var max_passes := maxi(pending_ids.size(), 1)
-	for _pass_index in range(max_passes):
-		if pending_ids.is_empty():
-			return
-
-		var progressed := false
-		var next_pending: Array[String] = []
-		for ability_id in pending_ids:
-			if _try_assign_position(tree, ability_id):
-				progressed = true
-			else:
-				next_pending.append(ability_id)
-
-		pending_ids = next_pending
-		if not progressed:
-			break
-
-	for ability_id in pending_ids:
-		if not _node_positions.has(ability_id):
-			_node_positions[ability_id] = Vector2.ZERO
-
-func _try_assign_position(tree: Dictionary, ability_id: String) -> bool:
-	if _node_positions.has(ability_id):
-		return true
-	if not tree.has(ability_id):
-		return false
-
-	var parent_id := _get_parent_id(tree, ability_id)
-	if parent_id == "":
-		_node_positions[ability_id] = Vector2.ZERO
-		return true
-	if not _node_positions.has(parent_id):
-		return false
-
-	var parent_node: Dictionary = tree[parent_id]
-	var child_ids: PackedStringArray = parent_node.get("children", PackedStringArray())
-	var child_count := maxi(child_ids.size(), 1)
-	var child_index := child_ids.find(ability_id)
-	if child_index < 0:
-		child_index = 0
-
-	var angle := -PI * 0.5
-	if child_count > 1:
-		angle = -PI * 0.5 + TAU * float(child_index) / float(child_count)
-
-	var parent_pos := _node_positions[parent_id] as Vector2
-	var direction := Vector2(cos(angle), sin(angle))
-	_node_positions[ability_id] = parent_pos + direction * EDGE_LENGTH
-	return true
-
-func _get_parent_id(tree: Dictionary, ability_id: String) -> String:
-	if not tree.has(ability_id):
-		return ""
-
-	var node_data: Dictionary = tree[ability_id]
-	var requires: PackedStringArray = node_data.get("requires", PackedStringArray())
-	for required_id in requires:
-		if tree.has(required_id):
-			return required_id
-	return ""
-
-func _fit_positions_to_canvas(positions: Dictionary) -> void:
-	if positions.is_empty():
-		custom_minimum_size = size
-		return
-
-	var min_x := INF
-	var max_x := -INF
-	var min_y := INF
-	var max_y := -INF
-
-	for pos_variant in positions.values():
-		var pos := pos_variant as Vector2
-		min_x = minf(min_x, pos.x)
-		max_x = maxf(max_x, pos.x)
-		min_y = minf(min_y, pos.y)
-		max_y = maxf(max_y, pos.y)
-
-	var tree_width := (max_x - min_x) + CANVAS_PADDING * 2.0 + NODE_DIAMETER
-	var tree_height := (max_y - min_y) + CANVAS_PADDING * 2.0 + NODE_DIAMETER
-	var canvas_size := Vector2(maxf(size.x, tree_width), maxf(size.y, tree_height))
-	custom_minimum_size = canvas_size
-
-	var offset := Vector2(
-		-min_x + CANVAS_PADDING + NODE_DIAMETER * 0.5,
-		-min_y + CANVAS_PADDING + NODE_DIAMETER * 0.5
-	)
-	var extra := (canvas_size - Vector2(tree_width, tree_height)) * 0.5
-	offset += extra
-
-	for ability_id_variant in positions.keys():
-		var ability_id := String(ability_id_variant)
-		positions[ability_id] = (positions[ability_id] as Vector2) + offset
-
-func _compute_subtree_weight(
-	tree: Dictionary,
-	ability_id: String,
-	subtree_weights: Dictionary,
-	visited: Dictionary
-) -> int:
-	if visited.has(ability_id):
-		return 1
-	visited[ability_id] = true
-
-	if not tree.has(ability_id):
-		return 1
-
-	var node_data: Dictionary = tree[ability_id]
-	var child_ids: Array[String] = []
-	for child_variant in node_data.get("children", PackedStringArray()):
-		var child_id := String(child_variant)
-		if tree.has(child_id):
-			child_ids.append(child_id)
-	child_ids.sort()
-
-	if child_ids.is_empty():
-		subtree_weights[ability_id] = 1
-		return 1
-
-	var total_weight := 0
-	for child_id in child_ids:
-		total_weight += _compute_subtree_weight(tree, child_id, subtree_weights, visited.duplicate())
-
-	var weight := maxi(total_weight, 1)
-	subtree_weights[ability_id] = weight
-	return weight
-
-func _assign_by_angle(
-	tree: Dictionary,
-	ability_id: String,
-	parent_angle: float,
-	span: float,
-	subtree_weights: Dictionary,
-	positions: Dictionary,
-	visited: Dictionary
-) -> void:
-	if visited.has(ability_id):
-		return
-	visited[ability_id] = true
-
-	if not tree.has(ability_id):
-		return
-
-	var node_data: Dictionary = tree[ability_id]
-	var child_ids: Array[String] = []
-	for child_variant in node_data.get("children", PackedStringArray()):
-		var child_id := String(child_variant)
-		if tree.has(child_id):
-			child_ids.append(child_id)
-	child_ids.sort()
-
-	var total_weight := 0.0
-	for child_id in child_ids:
-		total_weight += float(subtree_weights.get(child_id, 1))
-	if total_weight <= 0.0:
-		return
-
-	var child_span := maxf(span, MIN_SPAN)
-	var angle_cursor := parent_angle - child_span * 0.5
-	for child_id in child_ids:
-		var child_weight := float(subtree_weights.get(child_id, 1))
-		var share := child_span * (child_weight / total_weight)
-		var child_angle := angle_cursor + share * 0.5
-		var direction := Vector2(cos(child_angle), sin(child_angle))
-		positions[child_id] = (positions[ability_id] as Vector2) + direction * EDGE_LENGTH
-		_assign_by_angle(
-			tree,
-			child_id,
-			child_angle,
-			share * 0.85,
-			subtree_weights,
-			positions,
-			visited.duplicate()
-		)
-		angle_cursor += share
-
-func _draw_graph_edges(tree: Dictionary, positions: Dictionary) -> void:
-	for ability_id_variant in tree.keys():
-		var parent_id := String(ability_id_variant)
-		if not positions.has(parent_id):
-			continue
-
-		var parent_pos: Vector2 = positions[parent_id]
-		var node_data: Dictionary = tree[parent_id]
-		for child_variant in node_data.get("children", PackedStringArray()):
-			var child_id := String(child_variant)
-			if not positions.has(child_id):
-				continue
-
-			var child_pos: Vector2 = positions[child_id]
-			var line := Line2D.new()
-			line.width = 2.0
-			line.default_color = Color(0.78, 0.78, 0.82, 0.85)
-			line.add_point(parent_pos)
-			line.add_point(child_pos)
-			graph_edges.add_child(line)
-
-func _draw_graph_nodes(tree: Dictionary, positions: Dictionary) -> void:
-	for ability_id_variant in positions.keys():
-		var ability_id := String(ability_id_variant)
-		if not tree.has(ability_id):
-			continue
-
-		var node_data: Dictionary = tree[ability_id]
-		var ability_name := String(node_data.get("name", ability_id))
-		var is_unlocked := AbilityStore.is_unlocked(ability_id)
-		var is_expanded := AbilityStore.has_expanded(ability_id)
-
+	for i in range(_offered_cards.size()):
+		var card_value := int(_offered_cards[i])
 		var button := Button.new()
-		button.text = ""
-		button.tooltip_text = ability_name
-		button.custom_minimum_size = Vector2(NODE_DIAMETER, NODE_DIAMETER)
-		button.size = Vector2(NODE_DIAMETER, NODE_DIAMETER)
-		button.disabled = is_expanded
-		button.position = (positions[ability_id] as Vector2) - Vector2(NODE_DIAMETER * 0.5, NODE_DIAMETER * 0.5)
-		button.set(
-			"theme_override_styles/normal",
-			_make_circle_style(Color(0.18, 0.78, 0.40) if is_unlocked else Color(0.34, 0.36, 0.40))
-		)
-		button.set(
-			"theme_override_styles/hover",
-			_make_circle_style(Color(0.25, 0.86, 0.50) if is_unlocked else Color(0.44, 0.47, 0.53))
-		)
-		button.set(
-			"theme_override_styles/pressed",
-			_make_circle_style(Color(0.12, 0.62, 0.30) if is_unlocked else Color(0.24, 0.26, 0.30))
-		)
-		button.set("theme_override_styles/disabled", _make_circle_style(Color(0.14, 0.15, 0.17)))
+		button.custom_minimum_size = Vector2(140, 200)
+		button.text = str(card_value)
+		button.pressed.connect(_on_card_pressed.bind(i))
+		cards_row.add_child(button)
 
-		if not is_expanded:
-			button.pressed.connect(_on_ability_node_pressed.bind(ability_id))
+func _on_card_pressed(card_index: int) -> void:
+	if card_index < 0 or card_index >= _offered_cards.size():
+		return
 
-		graph_nodes.add_child(button)
+	var value := int(_offered_cards[card_index])
+	AbilityStore.add_selected_card(value)
+	_offered_cards.clear()
+	_render_cards()
 
-func _make_circle_style(fill_color: Color) -> StyleBoxFlat:
-	var style := StyleBoxFlat.new()
-	style.bg_color = fill_color
-	style.corner_radius_top_left = int(NODE_DIAMETER * 0.5)
-	style.corner_radius_top_right = int(NODE_DIAMETER * 0.5)
-	style.corner_radius_bottom_right = int(NODE_DIAMETER * 0.5)
-	style.corner_radius_bottom_left = int(NODE_DIAMETER * 0.5)
-	style.border_width_left = 2
-	style.border_width_top = 2
-	style.border_width_right = 2
-	style.border_width_bottom = 2
-	style.border_color = Color(0.95, 0.95, 0.95, 0.9)
-	return style
+func _refresh_selected_label() -> void:
+	var picked := AbilityStore.get_selected_cards()
+	selected_label.text = "Selected: %s" % [var_to_str(picked)]
 
-func _add_graph_message(text: String) -> void:
-	var label := Label.new()
-	label.text = text
-	label.position = Vector2(12, 12)
-	graph_nodes.add_child(label)
-
-func _on_ability_node_pressed(ability_id: String) -> void:
-	AbilityStore.add_random_children(ability_id)
+func _is_menu_visible() -> bool:
+	var node: Node = self
+	while node != null:
+		if node is CanvasLayer:
+			return (node as CanvasLayer).visible
+		node = node.get_parent()
+	return false
