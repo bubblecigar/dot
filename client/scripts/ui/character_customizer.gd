4ad259343d66d67f3b7a273f057bed8dbae90a28
roy.wang <roy.wang@goanimate.com>
2026-03-11 23:35:56 +0800

    Fix button signal connection for Godot 4.x using lambda; toggle state on click

--- client/scripts/ui/character_customizer.gd
+++ client/scripts/ui/character_customizer.gd
@@ -1,3 +1,17 @@
 extends Control
 
-# Blank template for character customization logic
+
+# Lookup for button states
+var button_states := {}
+
+func _ready():
+	for i in range(1, 10):
+		var btn = $MarginContainer/Grid.get_node("Button%d" % i)
+		button_states[btn.name] = false
+		btn.pressed.connect(func(): _on_button_pressed(btn))
+
+func _on_button_pressed(btn):
+	var name = btn.name
+	button_states[name] = !button_states[name]
+	# Optional: print state for debugging
+	print(button_states)
