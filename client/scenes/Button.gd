3322c9d01f467dd00dfff5c1e142516a03f0e25c
roy.wang <roy.wang@goanimate.com>
2026-03-13 00:07:12 +0800

    Fix button background color override to use 'bg_color' for state-based coloring

--- client/scenes/Button.gd
+++ client/scenes/Button.gd
@@ -22,7 +22,15 @@ func _on_button_pressed():
 	print("%s pressed, state: %s" % [name, button_state])
 
 func _update_color():
+	var style = StyleBoxFlat.new()
+
 	if button_state:
-		modulate = Color(0.3, 0.7, 1) # blue
+		add_theme_color_override("font_color", Color.WHITE)
+		style.bg_color = Color(0.3, 0.7, 1)
 	else:
-		modulate = Color(1, 1, 1) # default
+		add_theme_color_override("font_color", Color(0.3, 0.7, 1))
+		style.bg_color = Color.WHITE
+
+	add_theme_stylebox_override("normal", style)
+	add_theme_stylebox_override("hover", style)
+	add_theme_stylebox_override("pressed", style)
