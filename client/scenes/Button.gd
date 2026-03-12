d76441b02e386f2cc1145c614eec01df9226e8ce
roy.wang <roy.wang@goanimate.com>
2026-03-13 00:00:20 +0800

    Change button to update text color based on state (blue when active, white when inactive)

--- client/scenes/Button.gd
+++ client/scenes/Button.gd
@@ -14,7 +14,15 @@ func _init(label: String = "", state: bool = false, name: String = ""):
 
 func _ready():
 	pressed.connect(_on_button_pressed)
+	_update_color()
 
 func _on_button_pressed():
 	button_state = !button_state
+	_update_color()
 	print("%s pressed, state: %s" % [name, button_state])
+
+func _update_color():
+	if button_state:
+		modulate = Color(0.3, 0.7, 1) # blue
+	else:
+		modulate = Color(1, 1, 1) # default
