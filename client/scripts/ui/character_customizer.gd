6991a5877979ba9fffb24dd41a13354917ab7cba
roy.wang <roy.wang@goanimate.com>
2026-03-12 23:46:08 +0800

    Refactor CharacterCustomizer to use Button.tscn for dynamic button instancing

--- client/scripts/ui/character_customizer.gd
+++ client/scripts/ui/character_customizer.gd
@@ -5,10 +5,20 @@ extends Control
 var button_states := {}
 
 func _ready():
+	var grid = $MarginContainer/Grid
+	# Remove all children from the grid
+	for child in grid.get_children():
+		child.queue_free()
+	var ButtonScene = preload("res://client/scenes/Button.tscn")
 	for i in range(1, 10):
-		var btn = $MarginContainer/Grid.get_node("Button%d" % i)
+		var btn = ButtonScene.instantiate()
+		btn.name = "Button%d" % i
+		btn.text = "Button %d" % i
+		btn.size_flags_horizontal = 3
+		btn.size_flags_vertical = 3
 		button_states[btn.name] = false
 		btn.pressed.connect(func(): _on_button_pressed(btn))
+		grid.add_child(btn)
 
 func _on_button_pressed(btn):
 	var name = btn.name
